66e61f7a798a77a6bbcdcb2eefe572f98187a91f
Marvin <93428739@qq.com>
2024-12-18 11:04:48 +0800

    Support swiftpackage

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+    name: "LCAppSandboxFileKit",
+    defaultLocalization: "en", // 指定默认语言为英文
+    platforms: [
+        .macOS(.v10_15) // 指定支持的 macOS 版本
+    ],
+    products: [
+        .library(
+            name: "LCAppSandboxFileKit",
+            targets: ["LCAppSandboxFileKit"]),
+    ],
+    targets: [
+        .target(
+            name: "LCAppSandboxFileKit",
+            resources: [
+                .process("Resources") // 添加资源路径
+            ]),
+        .testTarget(
+            name: "LCAppSandboxFileKitTests",
+            dependencies: ["LCAppSandboxFileKit"]),
+    ]
+)
